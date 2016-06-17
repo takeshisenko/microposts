c04e4aeff4a554d0f9f2d6e059dd94602c72100e
senko <tsenko@netprotections.co.jp>
2016-06-17 09:38:08 +0000

    一覧OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,27 @@ class UsersController < ApplicationController
     end
   end
   
+   def following
+    @user = User.find(params[:id])
+    followingall = Relationship.where(follower_id: params[:id])
+    @following_users = Array.new
+    
+    followingall.each do |following|
+      p @following_users << User.find_by(id: following.followed_id)
+    end   
+  end
+  
+  def followers
+    @user  = User.find(params[:id])
+    followerall  = Relationship.where(followed_id: params[:id])
+    
+    p @follower_users = Array.new
+    
+    followerall.each do |follower|
+      @follower_users << User.find_by(id: follower.follower_id)
+    end
+  end
+
   # def new
   #   @user = User.new
   # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,10 @@ class User < ActiveRecord::Base
                                      foreign_key: "follower_id",
                                      dependent:   :destroy
   has_many :following_users, through: :following_relationships, source: :followed
-
+  has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :follower_users, through: :follower_relationships, source: :follower
   # 他のユーザーをフォローする
   def follow(other_user)
     following_relationships.find_or_create_by(followed_id: other_user.id)
@@ -27,4 +30,5 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+  
 end
